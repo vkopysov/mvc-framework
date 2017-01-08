8c54397643273f2d5c0dca574ae98da26bd8985e
vkopysov <vk.nure@gmail.com>
2017-01-08 17:15:47 -0500

    merge

---
+++ app/Models/User.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sparrow
+ * Date: 12/25/16
+ * Time: 10:57 AM
+ */
+
+namespace Models;
+
+class User
+{
+    public $username;
+
+    public $email;
+
+
+    /*
+    function __construct($name, $email)
+    {
+        $this->username = $name;
+        $this->email = $email;
+    }
+    */
+
+
+}
