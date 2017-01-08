03890b74cbe86c07f170ca0830671d98b97a9729
vkopysov <vk.nure@gmail.com>
2017-01-08 02:37:49 -0500

    View get data dynamically or as arrays

--- app/Core/View.php
+++ app/Core/View.php
@@ -8,46 +8,80 @@
 
 namespace Core;
 
+/**
+ * Class View
+ * Формирует шаблоны и подшаблоны представления
+ * @package Core
+ */
 class View
 {
+    /**
+     * @var string view  шаблоны оформления
+     */
     protected $template;
+    /**
+     * @var array данные из контроллера
+     */
     protected $data = [];
 
-    //создаем основной шаблон
-    public function __construct($template = 'main')
+    /**
+     * Создание основного шаблона страницы, Передаем темплейт и массив данных
+     * View constructor.
+     * @param string $template
+     */
+    public function __construct($template = 'main', array $data = null)
     {
-            $this->template = $this->loadFile($template);
+        $this->template = $this->loadFile($template);
+        if ($data !== null) {
+            foreach ($data as $key => $value) {
+                $this->data[$key] = $value;
+            }
+        }
     }
 
-    //формируем параметры (данные, подшаблоны)
+    /**
+     * Метод динамического создания параметров (данных и подшаблонов)
+     * @param $key
+     * @param $value
+     */
     public function __set($key, $value)
     {
         $this->data[$key] = $value;
     }
 
-     //формируем подшаблон с параметрами
+    /**
+     * Метод для создания подшаблона в основном шаблоне
+     * Подшаблон хранится как элемент массива data
+     * @param $templateBlock
+     * @param array|null $data
+     */
     public function block($templateBlock, array $data = null)
     {
-            if ($data !== null ) {
-                extract($data, EXTR_SKIP);
-            }
-            ob_start();
-            require $this->loadFile($templateBlock);
-            $out = ob_get_contents();
-             ob_end_clean();
-            //return $out;
-            $this->$templateBlock = $out;
-
+        if ($data !== null) {
+            extract($data, EXTR_SKIP);
+        }
+        ob_start();
+        require $this->loadFile($templateBlock);
+        $out = ob_get_contents();
+        ob_end_clean();
+        //return $out;
+        $this->$templateBlock = $out;
     }
 
-    //отображаем шаблон с параметрами
+    /**
+     * Подключение шаблона с параметрами и подшаблонами (если есть)
+     */
     public function render()
     {
         extract($this->data);
         require($this->template);
     }
 
-    //проверяем наличие шаблона
+    /**
+     * Проверка наличия файла шаблона
+     * @param $file
+     * @return string
+     */
     private function loadFile($file)
     {
         $path = ROOT.'/app/templates/'.$file.'.phtml';
@@ -55,7 +89,7 @@ class View
             return $path;
         } else {
             exit('File '.$path.' not found.');
+           // throw new \Exception("'Template file '.$path.' not found.'")
         }
-
     }
 }
