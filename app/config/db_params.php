45f6082b6baf574372027b17ff5d44faf8b15bfe
vkopysov <vk.nure@gmail.com>
2017-01-08 17:13:01 -0500

    ne db config array

--- app/config/db_params.php
+++ app/config/db_params.php
@@ -1,9 +1,15 @@
 <?php
-
+/*
 return array(
     'host' => 'localhost',
     'db_name' => 'eshopdb',
     'user' => 'root',
     'password' => 'admin',
-    );
+    );*/
 
+return array(
+    'dsn' => 'mysql:host=localhost; dbname=new_schema',
+    'user' => 'root',
+    'password' => 'admin',
+    'options' => []
+);
